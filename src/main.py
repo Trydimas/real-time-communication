09f1e57fefc6be7eac8ab5729f2b96232a5f6e6e
Trydimas <akentev.dima@bk.ru>
2025-04-05 19:43:08 +0300

    fill main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 from db import create_tables
+from controllers.message import router
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+app.include_router(router)
+
+
+
 
 if __name__ == "__main__":
     create_tables()
