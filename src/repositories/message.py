8161bef7dbaed692fef1f3b635e7dc6443c12744
Trydimas <akentev.dima@bk.ru>
2025-04-05 22:16:05 +0300

    add message methods repo

--- src/repositories/message.py
+++ src/repositories/message.py
@@ -1,4 +1,4 @@
-from schema.message import MesageResp
+from schema.message import MesageResp, MessageSend, MessageRefactor
 
 
 def get_all_messages() -> list[MesageResp]:
@@ -11,3 +11,15 @@ def get_messages_by_user(user_id: int) -> list[MesageResp]:
 
 def get_message_by_id(message_id: int) -> MesageResp:
     pass
+
+
+def send_message(body: MessageSend) -> MesageResp:
+    pass
+
+
+def delete_message(message_id: str) -> MesageResp:
+    pass
+
+def refactor_message(message_id: str, body: MessageRefactor) -> MesageResp:
+    pass
+
